2403f5e9ac0cc7bced1942f433a8894db1f27cf2
JugglypuffM <stas.klinin@gmail.com>
2024-05-24 12:50:24 +0500

    logger code duplication fix

--- src/main/java/log/Logger.java
+++ src/main/java/log/Logger.java
@@ -12,24 +12,24 @@ public final class Logger {
     private Logger() {
     }
 
+    private static String appendStacktrace(String strMessage, StackTraceElement[] stackTrace){
+        return strMessage
+                + "\nStacktrace:\n"
+                + Arrays.toString(stackTrace).replace(",", ",\n");
+    }
+
     public static void debug(String strMessage) {
         defaultLogSource.append(LogLevel.Debug, strMessage);
     }
     public static void debug(String strMessage, StackTraceElement[] stackTrace) {
-        defaultLogSource.append(LogLevel.Debug, strMessage
-                + "\n\nStacktrace:\n"
-                    + Arrays.toString(stackTrace).replace(",", ",\n")
-        );
+        defaultLogSource.append(LogLevel.Debug, appendStacktrace(strMessage, stackTrace));
     }
 
     public static void error(String strMessage) {
         defaultLogSource.append(LogLevel.Error, strMessage);
     }
     public static void error(String strMessage, StackTraceElement[] stackTrace) {
-        defaultLogSource.append(LogLevel.Error, strMessage
-                + "\nStacktrace:\n"
-                + Arrays.toString(stackTrace).replace(",", ",\n")
-        );
+        defaultLogSource.append(LogLevel.Error, appendStacktrace(strMessage, stackTrace));
     }
 
 
